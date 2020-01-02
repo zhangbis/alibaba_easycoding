006b6e83c018154dc0a68a5ac7b66bcd6944fa98
张弼嵩 <422256850@qq.com>
2020-01-02 23:33:45 +0800

    feat 创建项目

---
+++ easycoding/src/test/java/com/alibaba/easycoding/EasycodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.easycoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasycodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
